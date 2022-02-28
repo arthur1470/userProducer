8414295ad416d85ae4e2e41b59d8fc15c4ee40b9
Arthur Marques <arthur.l2@hotmail.com>
2022-02-28 18:17:39 -0300

    Updating docker compose, and setting up jwt token configuration

--- src/main/java/com/usuario/producer/security/ResourceServerConfig.java
+++ src/main/java/com/usuario/producer/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.usuario.producer.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +22,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 			.and()
 			.cors().and()
-			.oauth2ResourceServer().opaqueToken();
+			.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		var secretKey = new SecretKeySpec("dsadjosajdoiasjdojptwhqpfqwuihdpwqodjwqpofhwquighbqwbgpowjdwqiopdjwqphuwqiopfgwfwqfwqgqwgwqd".getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 }
